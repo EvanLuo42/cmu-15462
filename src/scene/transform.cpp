2b4b29b0540c3cf269a943e486b11fdec4a5b760
Evan Luo <zl0402@fastmail.com>
2025-02-04 14:06:05 +0800

    A1T1: Scene Functions

--- src/scene/transform.cpp
+++ src/scene/transform.cpp
@@ -12,15 +12,25 @@ Mat4 Transform::parent_to_local() const {
 Mat4 Transform::local_to_world() const {
 	// A1T1: local_to_world
 	//don't use Mat4::inverse() in your code.
+	auto local_to_parent_mat = local_to_parent();
 
-	return Mat4::I; //<-- wrong, but here so code will compile
+	if (auto parent_shared = parent.lock()) {
+		return parent_shared->local_to_world() * local_to_parent_mat;
+	}
+
+	return local_to_parent_mat;
 }
 
 Mat4 Transform::world_to_local() const {
 	// A1T1: world_to_local
 	//don't use Mat4::inverse() in your code.
+	auto parent_to_local_mat = parent_to_local();
 
-	return Mat4::I; //<-- wrong, but here so code will compile
+	if (auto parent_shared = parent.lock()) {
+		return parent_to_local_mat * parent_shared->world_to_local();
+	}
+
+	return parent_to_local_mat;
 }
 
 bool operator!=(const Transform& a, const Transform& b) {
